5e2fb3ef9911fa90e8ea0a96352645d674a5bb68
kenn3y <125267353+kenn3y@users.noreply.github.com>
2026-03-07 09:05:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ let noteAlreadyScored = false;
 let stableCount = 0;
 let lastCents = null;
 
-let realtimeFeedback = false;
+let realtimeFeedback = true;
 let currentTargetMidi = null;
 let finalCents = null;
 
